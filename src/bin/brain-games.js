fc28b04fa267843eb81d1603bc43338bc1f995fe
Adel <htwr86@gmail.com>
2019-04-10 18:41:47 +0300

    add travis Ci. step 3 completed

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
-import {askQuestion} from '..';
+import askQuestion from '..';
 
 askQuestion();
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import readlineSync from 'readline-sync';
 
-
-export const askQuestion = () => {
-    const name = readlineSync.question('What is your name?  ');
-    console.log(`Welcome to the Brain-Games! ${name}`);
+const askQuestion = () => {
+  const name = readlineSync.question('What is your name?  ');
+  console.log(`Welcome to the Brain-Games! ${name}`);
 };
+
+export default askQuestion;
