c7f2a44f1b283c662fb1bb33d487b3f0bb34cf14
Adel <htwr86@gmail.com>
2019-04-13 17:41:29 +0300

    1.8.4 -3  correct const secretNumber

--- src/games/progression.js
+++ src/games/progression.js
@@ -9,11 +9,10 @@ export const getQuestionAndAnswer = () => {
   const progressionLength = 10;
   const emptyElementPosition = getRandomInteger(2, progressionLength);
   let progression = '';
-  let secretNumber = 0;
+  const secretNumber = first + step * emptyElementPosition;
 
   for (let i = 0; i <= progressionLength; i += 1) {
     if (i === emptyElementPosition) {
-      secretNumber = first + step * i;
       progression += ' ..';
     } else {
       progression = `${progression} ${first + step * i}`;
